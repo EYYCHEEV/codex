58d46ff910b04d231120727886e3341a99ba087c
EYYCHEEV <eyycheev@gmail.com>
2026-03-05 15:10:50 -0800

    test: make feedback attachment diagnostics deterministic

--- codex-rs/feedback/src/lib.rs
+++ codex-rs/feedback/src/lib.rs
@@ -703,6 +703,7 @@ mod tests {
 
     use super::*;
     use crate::FeedbackDiagnostic;
+    use crate::FeedbackDiagnostics;
     use pretty_assertions::assert_eq;
     use tracing_subscriber::layer::SubscriberExt;
     use tracing_subscriber::util::SubscriberInitExt;
