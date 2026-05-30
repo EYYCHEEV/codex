3c5e966f805e6f33a9a9ba237092653b5c2eb241
EYYCHEEV <eyycheev@gmail.com>
2026-05-30 21:53:50 +0800

    test(app-server): avoid live skills watcher in unit tests

--- codex-rs/app-server/src/skills_watcher.rs
+++ codex-rs/app-server/src/skills_watcher.rs
@@ -38,6 +38,9 @@ impl SkillsWatcher {
         skills_service: Arc<SkillsService>,
         outgoing: Arc<OutgoingMessageSender>,
     ) -> Arc<Self> {
+        #[cfg(test)]
+        let file_watcher = Arc::new(FileWatcher::noop());
+        #[cfg(not(test))]
         let file_watcher = match FileWatcher::new() {
             Ok(file_watcher) => Arc::new(file_watcher),
             Err(err) => {
